8baf242e6a85e79d3e2c86666a3426ac88e367b1
JohnnyD5 <44303418+JohnnyD5@users.noreply.github.com>
2019-09-06 13:28:57 -0500

    c

--- LES_assignment_R/LES_process.py
+++ LES_assignment_R/LES_process.py
@@ -5,6 +5,7 @@ Created on Sun Jul 28 10:02:50 2019
 @author: zding5
 """
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 pd.set_option('precision', 6)
 pd.set_option('expand_frame_repr', True)
@@ -18,23 +19,26 @@ class LES():
                        "u3", "v3", "u4", "v4"], engine='python')
         pdf.to_hdf(path + '/cloud.h5',key='cloud')
         self.data = pd.read_hdf(path + '/cloud.h5', key='cloud')
-        #self.data['v_mag'] = (self.data['vx']**2 + self.data['vy']**2)**0.5
+        dt = 0.0008178
+        self.data['t'] = np.arrange(dt, dt*len(self.data), dt)
 
-    def plot_angularV_over_t(self, t = None, y = None):
+    def time_history_plot(self, t = None, y1 = None, y2 = None):
         # Plot the angular velocity of the ellipse over time
         if t is None:
             t = self.data['t']
-        if y is None:
-            y = abs(self.data['wz'])
+        if y1 is None:
+            y1 = abs(self.data['u1'])
+        if y2 is None:
+            y2 = abs(self.data['v1'])
         fig, ax = plt.subplots(figsize=(6, 5))
-        ax.plot(t, y)
+        ax.plot(t, y1, t, y2)
         ax.set_xlabel('t')
-        ax.set_ylabel(r'$\hat{\omega}$')
+        ax.set_ylabel('v')
         #removing top and right borders
         ax.spines['top'].set_visible(False)
         ax.spines['right'].set_visible(False)
         # set x limit and y limit
-        ax.set_xlim(left = 0, right = 300)
+        #ax.set_xlim(left = 0, right = 300)
         plt.show()
         return
 
@@ -52,5 +56,5 @@ if __name__ == '__main__':
     path = ''.join(path)
     ###
     case = LES(path)
-    #case.plot_angularV_over_t()
-    case.test()
+    case.time_history_plot()
+    #case.test()
